4297149144a4eddf5d3533a75232577bbbda07e0
A <ashley@evatt.ai>
2025-10-30 14:38:00 +0800

    adding serializers, views etc

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework import viewsets
+from .models import Item
+from .serializers import ItemSerializer
+
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all().order_by("id")
+    serializer_class = ItemSerializer
