8fb0df8e4339c1cc1bf6590901b950e6a5726635
A <ashley@evatt.ai>
2025-11-04 19:43:04 +0800

    updating

--- frontend-agriculturalmanagementsoftware/src/App.jsx
+++ frontend-agriculturalmanagementsoftware/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1> Agriculture Management Software by Ashley Edgar </h1>
+      <h1> Agriculture Management Software by Ashley Edgar - excited to get this done </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
